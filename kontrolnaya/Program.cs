b47bc42b940691f29e959639d0e13df7373eef56
Виктория <nikulkina93@list.ru>
2022-11-11 18:40:38 +0300

    Добавлено сама программа решающая посталенную задачу

---
+++ kontrolnaya/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+ 
+public class Example
+{
+    public static void Main()
+    {
+        //Собираем исходный массив
+        string[] array = new string[]{};        
+        array = array.Append("dfgfd").ToArray();
+        array = array.Append("12").ToArray();
+        array = array.Append("sss").ToArray();
+        array = array.Append("3453").ToArray();        
+        array = array.Append("35d").ToArray();
+           
+        //Выводим исходный массив
+        Console.Write("['");
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            Console.Write(array[i] + "', '");
+        }
+        Console.Write(array[array.Length-1] + "'] -> ");
+
+        //Собираем результирующий массив
+        string[] result = new string[]{};
+        for (int i = 0; i <= array.Length-1; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                result = result.Append(array[i]).ToArray();
+            }
+        }
+
+        //Выводим результат
+        Console.Write("['");
+        for (int i = 0; i < result.Length-1; i++)
+        {
+            Console.Write(result[i] + "', '");
+        }
+        Console.Write(result[result.Length-1] + "']");
+    }
+}
